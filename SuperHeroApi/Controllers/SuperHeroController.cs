eedb51a9dcf59fb658d475442f7763730be0b4af
Taufik Herjanto <taufik.herjanto@live.com>
2022-06-10 15:30:46 +0700

    Add update and delete

--- SuperHeroApi/Controllers/SuperHeroController.cs
+++ SuperHeroApi/Controllers/SuperHeroController.cs
@@ -14,6 +14,22 @@ namespace SuperHeroApi.Controllers
                 FirstName = "Peter",
                 LastName = "Parker",
                 Place = "New York City"
+            },
+            new SuperHero
+            {
+                Id = 2,
+                Name = "Batman",
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                Place = "Gotham City"
+            },
+            new SuperHero
+            {
+                Id = 3,
+                Name = "Iron Man",
+                FirstName = "Tony",
+                LastName = "Stark",
+                Place = "New York City"
             }
         };
 
@@ -23,11 +39,46 @@ namespace SuperHeroApi.Controllers
             return Ok(heroes);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+            return Ok(hero);
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
             heroes.Add(hero);
             return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+            
+            return Ok(hero);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SuperHero>> Delete(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+
+            heroes.Remove(hero);
+            return Ok(hero);
         }
     }
 }
